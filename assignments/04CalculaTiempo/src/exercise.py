52c32a63dd8b3ac6114492b2ece458624fdabf97
juanpml <A01742169@itesm.mx>
2021-08-14 01:56:29 +0000

    Tareas

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = int(input("Dame tu edad: "))
+    a = int(input("Dame el año actual: "))
+    final = (a - edad) + 100
+    print("Cumplirás 100 años en el año:",final)
+
 
 
 
--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    nace = int(input("Dame el año de nacimiento: "))
+    actual = int(input("Dame el año actual: "))
+    lustros = (actual-nace)/5
+    print("Los lustros que has vivido son:",lustros)
+    
 
 
 
